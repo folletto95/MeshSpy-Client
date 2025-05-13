a223dc14fc7dce03ebeb98316c36e94ada7b0c29
folletto95 <malenottinicco@gmail.com>
2025-05-13 17:05:33 +0200

    modified:   meshspy-ui/src/App.jsx 	modified:   meshspy-ui/src/main.jsx

--- meshspy-ui/src/App.jsx
+++ meshspy-ui/src/App.jsx
@@ -1,21 +1,24 @@
+import React from "react";
 import Sidebar from "./components/Sidebar";
+import MapView from "./components/MapView";
 import Topbar from "./components/Topbar";
 import Metrics from "./components/Metrics";
-import MapView from "./components/MapView";
 import LogViewer from "./components/LogViewer";
+import WifiDialog from "./components/WifiDialog";
 
 export default function App() {
   return (
-    <div className="flex h-screen bg-gray-50">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
-        <Topbar />
-        <main className="flex-1 p-4 space-y-4 overflow-auto">
+    <div className="h-screen flex flex-col">
+      <Topbar />
+      <div className="flex flex-1">
+        <Sidebar />
+        <div className="flex flex-col flex-1">
           <Metrics />
           <MapView />
-          <LogViewer />
-        </main>
+        </div>
       </div>
+      <WifiDialog />
+      <LogViewer />
     </div>
   );
 }

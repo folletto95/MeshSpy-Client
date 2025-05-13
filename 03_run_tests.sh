2ba159adf05650d18f851da400684a3628255929
folletto95 <malenottinicco@gmail.com>
2025-05-13 16:50:23 +0200

    modified:   03_run_tests.sh 	modified:   tests/test_main.py

--- 03_run_tests.sh
+++ 03_run_tests.sh
@@ -3,10 +3,8 @@ set -euo pipefail
 
 source backend/.venv/bin/activate
 
-echo "▶️   Lancio pytest…"
-
-# Imposta il path per trovare correttamente 'backend'
-PYTHONPATH=. poetry run pytest
+echo "▶️  Lancio pytest…"
+export PYTHONPATH="$PWD"
 pytest -q
 
 echo "✅  Tutti i test sono passati."

4931eca8420b5c78823696f525386fc45a533902
folletto95 <malenottinicco@gmail.com>
2025-05-15 15:46:20 +0200

    modified:   meshspy-ui/src/main.jsx

--- meshspy-ui/src/main.jsx
+++ meshspy-ui/src/main.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { MapProvider } from "./lib/MapContext";
+import { MapProvider } from "./lib/MapContext.jsx";
+
+// (Facoltativo) Carica preferenza dark mode da localStorage
+if (localStorage.theme === "dark") {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

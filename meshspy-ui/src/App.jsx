7fa3a58cadd7997ebb207187014b43b9062da9eb
folletto95 <malenottinicco@gmail.com>
2025-05-19 12:05:52 +0200

    modified:   meshspy-ui/src/App.jsx

--- meshspy-ui/src/App.jsx
+++ meshspy-ui/src/App.jsx
@@ -19,7 +19,7 @@ export default function App() {
           <Metrics />
           <MapView />
           <LogViewer />
-           <StatusBanner />
+          <StatusBanner />
         </main>
       </div>
     </div>

52a46cbcf31e5a72b36611c1eb087ea3636ea223
folletto95 <malenottinicco@gmail.com>
2025-05-20 11:55:43 +0200

    modified:   backend/main.py 	modified:   client/meshtastic_utils.py 	modified:   client/rest_api.py

--- backend/main.py
+++ backend/main.py
@@ -141,7 +141,8 @@ async def request_position(data: RequestLocation, svc=Depends(get_mqtt_service))
     topic = f"mesh/request/{data.node_id}/location"
     payload = json.dumps({"cmd": "request_position", "from": "Server-MeshSpy"})
 
-    if not svc.client or not getattr(svc.client, "is_connected", True):
+    if not svc.connected:
+
         logger.warning(svc)
         logger.warning("⛔ MQTT non pronto, rifiuto comando.")
         raise HTTPException(status_code=503, detail="MQTT client not ready")
